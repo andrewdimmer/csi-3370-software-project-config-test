9bc0a6bb3b497d878d16f99b95a74f08f264f977
Andrew Dimmer <contactandrewd@gmail.com>
2020-10-25 19:32:23 -0400

    Super simple test

--- source/edu/oakand/test/middleware01/Junit5ExampleTest.java
+++ source/edu/oakand/test/middleware01/Junit5ExampleTest.java
@@ -1,29 +1,10 @@
 package edu.oakland.test.middleware01;
 
-import org.junit.jupiter.api;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
 @DisplayName("JUnit 5 Example")
 public class Junit5ExampleTest {
-
-  @BeforeAll
-  static void beforeAll() {
-    System.out.println("Before all test methods");
-  }
-
-  @BeforeEach
-  void beforeEach() {
-    System.out.println("Before each test method");
-  }
-
-  @AfterEach
-  void afterEach() {
-    System.out.println("After each test method");
-  }
-
-  @AfterAll
-  static void afterAll() {
-    System.out.println("After all test methods");
-  }
 
   @Test
   @DisplayName("First test")
