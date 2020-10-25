e4d5267fb2ae432486fa5ff08f06109d06ecb5bf
Andrew Dimmer <contactandrewd@gmail.com>
2020-10-25 19:34:38 -0400

    fixed lexicographical import order error

--- source/edu/oakand/test/middleware01/Junit5ExampleTest.java
+++ source/edu/oakand/test/middleware01/Junit5ExampleTest.java
@@ -1,7 +1,7 @@
 package edu.oakland.test.middleware01;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("JUnit 5 Example")
 public class Junit5ExampleTest {
