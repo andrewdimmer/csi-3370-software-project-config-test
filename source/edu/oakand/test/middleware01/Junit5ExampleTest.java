df5d747bc8f816d260415fd50ce32bbebac16685
Andrew Dimmer <contactandrewd@gmail.com>
2020-10-25 19:49:49 -0400

    Exact imports for all annotations

--- source/edu/oakand/test/middleware01/Junit5ExampleTest.java
+++ source/edu/oakand/test/middleware01/Junit5ExampleTest.java
@@ -1,10 +1,34 @@
 package edu.oakland.test.middleware01;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("JUnit 5 Example")
 public class Junit5ExampleTest {
+
+  @BeforeAll
+  static void beforeAll() {
+    System.out.println("Before all test methods");
+  }
+
+  @BeforeEach
+  void beforeEach() {
+    System.out.println("Before each test method");
+  }
+
+  @AfterEach
+  void afterEach() {
+    System.out.println("After each test method");
+  }
+
+  @AfterAll
+  static void afterAll() {
+    System.out.println("After all test methods");
+  }
 
   @Test
   @DisplayName("First test")
