7ac281089b1368277dbc5aa75cb30537681e11ea
Andrew Dimmer <contactandrewd@gmail.com>
2020-10-26 02:32:22 -0400

    Added Tests to Add Z to Point

--- source/edu/oakand/test/middleware01/PointTest.java
+++ source/edu/oakand/test/middleware01/PointTest.java
@@ -11,6 +11,7 @@ public class PointTest {
 
   private int coordX = (int) (Math.random() * 100);
   private int coordY = (int) (Math.random() * 100);
+  private int coordZ = (int) (Math.random() * 100);
 
   @Test
   @DisplayName("coordX stored correctly")
@@ -24,5 +25,12 @@ public class PointTest {
   void coordyEqual() {
     Point p = new Point(coordX, coordY);
     assertEquals(p.getY(), coordY);
+  }
+
+  @Test
+  @DisplayName("coordZ stored correctly")
+  void coordyEqual() {
+    Point p = new Point(coordX, coordY, coordZ);
+    assertEquals(p.getZ(), coordZ);
   }
 }
