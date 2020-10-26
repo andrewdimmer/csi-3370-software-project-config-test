14b0a163882486b616b18048d4b1364916ab2109
Andrew Dimmer <contactandrewd@gmail.com>
2020-10-26 01:00:44 -0400

    Create Point.java

---
+++ source/edu/oakand/helper/middleware01/Point.java
@@ -0,0 +1,12 @@
+package edu.oakland.helper.middleware01;
+
+public class Point {
+
+    public int x;
+    public int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = x
+     }
+}
