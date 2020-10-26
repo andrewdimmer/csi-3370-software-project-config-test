b156f947c2d6dbb87a986c68aa87f0221ce009b4
Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
2020-10-26 02:12:34 -0400

    [Middleware_01] Create point class (#26)
    * Added the PointTest class
    
    This is a test class.
    
    * Create Point.java
    
    * Update documents to match checkstyle
    
    * Add semicolon
    
    * Added Type Cast from double to int
    
    * Fix access issue and whitespace
    
    * Made point vars private
    
    * Apply suggestions from code review to add getters
    
    Co-authored-by: Nathan Dimmer <nathandimmer@gmail.com>
    
    * Fix braces
    
    Co-authored-by: Nathan Dimmer <nathandimmer@gmail.com>

---
+++ source/edu/oakand/helper/middleware01/Point.java
@@ -0,0 +1,21 @@
+package edu.oakland.helper.middleware01;
+
+public class Point {
+
+  private int coordX;
+  private int coordY;
+
+  public Point(int coordX, int coordY) {
+    this.coordX = coordX;
+    this.coordY = coordY;
+  }
+
+  public int getX() {
+    return coordX;
+  }
+    
+  public int getY() {
+    return coordY;
+  }
+  
+}
---
+++ source/edu/oakand/test/middleware01/PointTest.java
@@ -0,0 +1,28 @@
+package edu.oakland.test.middleware01;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import edu.oakland.helper.middleware01.Point;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("JUnit 5 Point Test")
+public class PointTest {
+
+  private int coordX = (int) (Math.random() * 100);
+  private int coordY = (int) (Math.random() * 100);
+
+  @Test
+  @DisplayName("coordX stored correctly")
+  void coordxEqual() {
+    Point p = new Point(coordX, coordY);
+    assertEquals(p.getX(), coordX);
+  }
+
+  @Test
+  @DisplayName("coordY stored correctly")
+  void coordyEqual() {
+    Point p = new Point(coordX, coordY);
+    assertEquals(p.getY(), coordY);
+  }
+}
