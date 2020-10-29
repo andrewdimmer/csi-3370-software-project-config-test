2d10aab79db8fb87324aaaf1f5dadca0259db803
MathewYaldo <55512598+MathewYaldo@users.noreply.github.com>
2020-10-29 17:06:53 -0400

    Fixes 8

--- source/edu/oakand/test/database/CargoContainerTest.java
+++ source/edu/oakand/test/database/CargoContainerTest.java
@@ -1,8 +1,11 @@
 package edu.oakland.test.database;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.Random;
+
 import edu.oakland.helper.database.CargoContainer;
+
+import java.util.Random;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
