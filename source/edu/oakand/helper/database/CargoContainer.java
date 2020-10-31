9c4976b7b9f9012e57bdb9e9b8dd0548a1cd4906
MathewYaldo <55512598+MathewYaldo@users.noreply.github.com>
2020-10-31 18:59:13 -0400

    Created CargoContainer class plus tests (#34)
    * Create database folder, add CargoContainer class
    
    * General style fixes
    
    * .gitignore fix
    
    * remove .ds_store files

---
+++ source/edu/oakand/helper/database/CargoContainer.java
@@ -0,0 +1,48 @@
+package edu.oakland.helper.database;
+
+public class CargoContainer {
+
+  private String color;
+  private String location;
+  private String content;
+  private int containerId;
+
+  /**
+  * Main cargo container class. Used for testing uploading and using pull requests.
+
+  * @author Database Team
+  *
+  * @version 1.0
+  */
+  public CargoContainer(String color, String location, String content, int containerId) {
+    this.color = color;
+    this.location = location;
+    this.content = content;
+    this.containerId = containerId;
+  }
+
+  public String returnColor() {
+    return color;
+  }
+    
+  public String returnLocation() {
+    return location;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public String isContainer() {
+    return "Yes.";
+  }
+
+  public int returnId() {
+    return containerId;
+  }
+
+  public String returnProperties() {
+    return "C: " + color + " L: " + location + " cId: " + String.valueOf(containerId);
+  }
+  
+}
