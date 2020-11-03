dd23f612dce23be5d2ac0087692f1b213111ec38
Endrit <endritzenuni112@gmail.com>
2020-11-03 16:56:44 -0500

    Create EndritZenuniTest.java
    Test Practice

---
+++ source/edu/oakand/test/database/EndritZenuniTest.java
@@ -0,0 +1,32 @@
+package edu.oakland.test.database;
+
+import edu.oakland.helper.database.EndritZenuni;
+import jdk.internal.jline.internal.TestAccessible;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("EndritZenuni Test")
+public class EndritZenuniTest {
+    
+    private int randNum = (int)(Math.random() * 10);
+    private int randNum2 = (int)(Math.random() * 10);
+
+
+    @Test
+    @DisplayName("Add function")
+    void addEqual(){
+        EndritZenuni ez = new Mathematics(randNum, randNum2);
+        assertEquals(ez.add(), randNum + randNum2);
+    }
+
+    @Test
+    @DisplayName("subtract function")
+    void subtractEqual(){
+        EndritZenuni ez = new Mathematics(randNum, randNum2);
+        assertEquals(ez.add(), randNum - randNum2);
+    }
+
+
+}
