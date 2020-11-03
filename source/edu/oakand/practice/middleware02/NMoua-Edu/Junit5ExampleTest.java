4818fd180ea1af607752866e46cc743de8553699
NMoua-Edu <73411759+NMoua-Edu@users.noreply.github.com>
2020-11-03 17:36:50 -0500

    Update Junit5ExampleTest.java

--- source/edu/oakand/practice/middleware02/NMoua-Edu/Junit5ExampleTest.java
+++ source/edu/oakand/practice/middleware02/NMoua-Edu/Junit5ExampleTest.java
@@ -1,8 +1,8 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("JUnit 5 Example")
