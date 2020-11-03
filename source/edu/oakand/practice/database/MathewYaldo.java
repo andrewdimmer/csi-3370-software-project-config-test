5d04737dacd1877057088cf6715d556ccfd8bd7f
MathewYaldo <55512598+MathewYaldo@users.noreply.github.com>
2020-11-02 22:37:05 -0500

    Update 789

--- source/edu/oakand/practice/database/MathewYaldo.java
+++ source/edu/oakand/practice/database/MathewYaldo.java
@@ -1,18 +1,23 @@
 import org.junit.jupiter.api.*;
+
 @DisplayName("JUnit 5 Example")
-class JUnit5ExampleTest {
+class MathewYaldo {
+  
   @BeforeAll
   static void beforeAll() {
     System.out.println("Before all test methods");
   }
+
   @BeforeEach
   void beforeEach() {
     System.out.println("Before each test method");
   }
+
   @AfterEach
   void afterEach() {
     System.out.println("After each test method");
   }
+
   @AfterAll
   static void afterAll() {
     System.out.println("After all test methods");
