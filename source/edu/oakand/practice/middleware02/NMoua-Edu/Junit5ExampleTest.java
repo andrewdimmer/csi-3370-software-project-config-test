ed6b9b51aefab6a26284b99f4664c7c993e37a28
NMoua-Edu <73411759+NMoua-Edu@users.noreply.github.com>
2020-11-03 16:03:13 -0500

    Update Junit5ExampleTest.java

--- source/edu/oakand/practice/middleware02/NMoua-Edu/Junit5ExampleTest.java
+++ source/edu/oakand/practice/middleware02/NMoua-Edu/Junit5ExampleTest.java
@@ -5,33 +5,33 @@ class Junit5ExampleTest {
 
   @BeforeAll 
     static void beforeAll() {
-      System.out.println("Before all test method");
+    System.out.println("Before all test method");
   }
 
   @BeforeEach
     void beforeEach() {
-      System.out.println("Before each test method");
+    System.out.println("Before each test method");
   }
 
   @AfterEach
     void aftereach() {
-      System.out.println("After each test method");
+    System.out.println("After each test method");
   }
 
   @AfterAll
     static void afterall() {
-      System.out.println("After all test method");
+    System.out.println("After all test method");
   }
 
   @Test 
   @DisplayName("First test")
     void firstTest() {
-      System.out.println("First test method");
+    System.out.println("First test method");
   }
 
   @Test 
   @DisplayName("Second test")
     void secondTest() {
-      System.out.println("Second test method");
-	}
+    System.out.println("Second test method");
+  }
 }
