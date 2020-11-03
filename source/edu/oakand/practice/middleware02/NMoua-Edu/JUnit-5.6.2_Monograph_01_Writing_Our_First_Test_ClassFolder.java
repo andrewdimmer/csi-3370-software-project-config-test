70a7470083b7378fb2e7ce25161c66cbd126ca8a
NMoua-Edu <73411759+NMoua-Edu@users.noreply.github.com>
2020-11-03 15:54:01 -0500

    Update JUnit-5.6.2_Monograph_01_Writing_Our_First_Test_ClassFolder.java

--- source/edu/oakand/practice/middleware02/NMoua-Edu/JUnit-5.6.2_Monograph_01_Writing_Our_First_Test_ClassFolder.java
+++ source/edu/oakand/practice/middleware02/NMoua-Edu/JUnit-5.6.2_Monograph_01_Writing_Our_First_Test_ClassFolder.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.*;
 
-@DisplayName("JUnit 5 Example")
+@DisplayName("JUnit-5.6.2_Monograph_01_Writing_Our_First_Test_ClassFolder")
 class Junit5ExampleTest {
 
   @BeforeAll 
