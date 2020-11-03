85c8f10879f555d42732466f05781088b9037d92
MathewYaldo <55512598+MathewYaldo@users.noreply.github.com>
2020-11-02 22:34:56 -0500

    Indent

--- source/edu/oakand/practice/database/MathewYaldo.java
+++ source/edu/oakand/practice/database/MathewYaldo.java
@@ -1,20 +1,20 @@
 import org.junit.jupiter.api.*;
 @DisplayName("JUnit 5 Example")
 class JUnit5ExampleTest {
-@BeforeAll
-static void beforeAll() {
-System.out.println("Before all test methods");
-}
-@BeforeEach
-void beforeEach() {
-System.out.println("Before each test method");
-}
-@AfterEach
-void afterEach() {
-System.out.println("After each test method");
-}
-@AfterAll
-static void afterAll() {
-System.out.println("After all test methods");
-}
+  @BeforeAll
+  static void beforeAll() {
+    System.out.println("Before all test methods");
+  }
+  @BeforeEach
+  void beforeEach() {
+    System.out.println("Before each test method");
+  }
+  @AfterEach
+  void afterEach() {
+    System.out.println("After each test method");
+  }
+  @AfterAll
+  static void afterAll() {
+    System.out.println("After all test methods");
+  }
 }
