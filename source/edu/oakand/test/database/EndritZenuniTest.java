2a00999299438448a5296e4cef1ac34487d712d8
Endrit <endritzenuni112@gmail.com>
2020-11-03 17:13:17 -0500

    Update EndritZenuniTest.java
    indents and import static class by itself

--- source/edu/oakand/test/database/EndritZenuniTest.java
+++ source/edu/oakand/test/database/EndritZenuniTest.java
@@ -10,23 +10,23 @@ import org.junit.jupiter.api.Test;
 @DisplayName("EndritZenuni Test")
 public class EndritZenuniTest {
     
-    private int randNum = (int)(Math.random() * 10);
-    private int randNum2 = (int)(Math.random() * 10);
+  private int randNum = (int)(Math.random() * 10);
+  private int randNum2 = (int)(Math.random() * 10);
 
 
-    @Test
-    @DisplayName("Add function")
-    void addEqual(){
-        EndritZenuni ez = new Mathematics(randNum, randNum2);
-        assertEquals(ez.add(), randNum + randNum2);
-    }
+  @Test
+  @DisplayName("Add function")
+  void addEqual(){
+    EndritZenuni ez = new Mathematics(randNum, randNum2);
+    assertEquals(ez.add(), randNum + randNum2);
+  }
 
-    @Test
-    @DisplayName("subtract function")
-    void subtractEqual(){
-        EndritZenuni ez = new Mathematics(randNum, randNum2);
-        assertEquals(ez.add(), randNum - randNum2);
-    }
+  @Test
+  @DisplayName("subtract function")
+  void subtractEqual(){
+      EndritZenuni ez = new Mathematics(randNum, randNum2);
+      assertEquals(ez.add(), randNum - randNum2);
+  }
 
 
 }
