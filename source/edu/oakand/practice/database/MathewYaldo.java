3912503c51c12bf74363989380c0792dec35222e
MathewYaldo <55512598+MathewYaldo@users.noreply.github.com>
2020-11-02 22:43:20 -0500

    Test #378676276289837846

--- source/edu/oakand/practice/database/MathewYaldo.java
+++ source/edu/oakand/practice/database/MathewYaldo.java
@@ -1,15 +1,15 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 
 @DisplayName("JUnit 5 Example")
 class MathewYaldo {
