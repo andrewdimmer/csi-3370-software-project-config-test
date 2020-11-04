79a287149f5319c6955616882da946d4adc6794f
Nik Bergamin <nbergamin@oakland.edu>
2020-11-04 16:39:35 -0500

    Update JunitPractice.java
    changed formatting

--- source/edu/oakand/practice/middleware02/nbergamin123/JunitPractice.java
+++ source/edu/oakand/practice/middleware02/nbergamin123/JunitPractice.java
@@ -6,32 +6,32 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Junit Practice")
-class JunitPractice{
+class JunitPractice {
+    
+ @BeforeAll
+ static void beforeAll() {
+  System.out.println("Before all test methods");
+ }
 	
-	@BeforeAll
-	static void beforeAll() {
-		System.out.println("Before all test methods");
-	}
+ @BeforeEach
+ static void beforeEach() {
+  System.out.println("Before each test method");
+ }
 	
-	@BeforeEach
-	static void beforeEach() {
-		System.out.println("Before each test method");
-	}
+ @AfterEach
+ static void afterEach() {
+  System.out.println("After each test method");
+ }
 	
-	@AfterEach
-	static void afterEach() {
-		System.out.println("After each test method");
-	}
+ @Test
+ @DisplayName("First test")
+ void firstTest() {
+  System.out.println("First test method");
+ }
 	
-	@Test
-	@DisplayName("First test")
-	void firstTest() {
-		System.out.println("First test method");
-	}
-	
-	@Test
-	@DisplayName("Second test")
-	void secondTest() {
-		System.out.println("Second test method");
-	}
+ @Test
+ @DisplayName("Second test")
+ void secondTest() {
+  System.out.println("Second test method");
+ }
 }
