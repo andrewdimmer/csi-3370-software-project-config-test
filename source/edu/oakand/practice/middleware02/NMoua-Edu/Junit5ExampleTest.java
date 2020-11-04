975f1a342671ec25a32bf332642538b7036f1f92
NMoua-Edu <73411759+NMoua-Edu@users.noreply.github.com>
2020-11-03 19:20:45 -0500

    [Middleware02] Github Practice (#52)
    * Practice
    
    Github Practice
    
    * Update JUnit-5.6.2_Monograph_01_Writing_Our_First_Test_ClassFolder.java
    
    * Update JUnit-5.6.2_Monograph_01_Writing_Our_First_Test_ClassFolder.java
    
    * Update JUnit-5.6.2_Monograph_01_Writing_Our_First_Test_ClassFolder.java
    
    * Update JUnit-5.6.2_Monograph_01_Writing_Our_First_Test_ClassFolder.java
    
    * Update JUnit-5.6.2_Monograph_01_Writing_Our_First_Test_ClassFolder.java
    
    * Update JUnit-5.6.2_Monograph_01_Writing_Our_First_Test_ClassFolder.java
    
    * Create Junit5ExampleTest.java
    
    * Delete JUnit-5.6.2_Monograph_01_Writing_Our_First_Test_ClassFolder.java
    
    * Update Junit5ExampleTest.java
    
    * Update Junit5ExampleTest.java
    
    * Update Junit5ExampleTest.java

---
+++ source/edu/oakand/practice/middleware02/NMoua-Edu/Junit5ExampleTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("JUnit 5 Example")
+class Junit5ExampleTest {
+
+  @BeforeAll 
+    static void beforeAll() {
+    System.out.println("Before all test method");
+  }
+
+  @BeforeEach
+    void beforeEach() {
+    System.out.println("Before each test method");
+  }
+
+  @AfterEach
+    void aftereach() {
+    System.out.println("After each test method");
+  }
+
+  @AfterAll
+    static void afterall() {
+    System.out.println("After all test method");
+  }
+
+  @Test 
+  @DisplayName("First test")
+    void firstTest() {
+    System.out.println("First test method");
+  }
+
+  @Test 
+  @DisplayName("Second test")
+    void secondTest() {
+    System.out.println("Second test method");
+  }
+}
