f4406a838a6984e948492c065379393bcd7d87ce
jmccloud827 <60444521+jmccloud827@users.noreply.github.com>
2020-11-03 21:26:52 -0500

    Update JUnit5ExampleTest.java

--- source/edu/oakand/practice/display_02/jmccloud827/JUnit5ExampleTest.java
+++ source/edu/oakand/practice/display_02/jmccloud827/JUnit5ExampleTest.java
@@ -7,36 +7,36 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("JUnit 5 Example")
 class Junit5ExampleTest {
-	
-	@BeforeAll 
-	static void beforeAll() {
-		System.out.println("Before all test method");
-	}
 
-	@BeforeEach
-	void beforeEach() {
-		System.out.println("Before each test method");   
-	}
+  @BeforeAll 
+  static void beforeAll() {
+    system.out.println("Before all test method");
+  }
 
-	@AfterEach
-	void aftereach() {
-		System.out.println("After each test method");   
-	}
+  @BeforeEach
+  void beforeEach() {
+    system.out.println("Before each test method");   
+  }
 
-	@AfterAll
-	static void afterall() {
-		System.out.println("After all test method");   
-	}
+  @AfterEach
+  void aftereach() {
+    system.out.println("After each test method");   
+  }
 
-	@Test 
-	@DisplayName("First test")
-	void firstTest() {
-		System.out.println("First test method");   
-	}
+  @AfterAll
+  static void afterall() {
+    system.out.println("After all test method");   
+  }
 
-	@Test 
-	@DisplayName("Second test")
-	void secondTest() {
-		System.out.println("Second test method");
-	}
+  @Test 
+  @DisplayName("First test")
+  void firstTest() {
+    system.out.println("First test method");   
+  }
+
+  @Test 
+  @DisplayName("Second test")
+  void secondTest() {
+    system.out.println("Second test method");
+  }
 }
