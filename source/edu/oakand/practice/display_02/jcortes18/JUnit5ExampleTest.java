9ea8d8d126154b2a06a2c10fa41da3d397a55099
Jessica Cortes <jcortes@oakland.edu>
2020-11-04 19:52:22 -0500

    Update JUnit5ExampleTest.java

--- source/edu/oakand/practice/display_02/jcortes18/JUnit5ExampleTest.java
+++ source/edu/oakand/practice/display_02/jcortes18/JUnit5ExampleTest.java
@@ -5,33 +5,39 @@ class Junit5exampletest {
 
   @BeforeAll
   static void beforeAll(){
+
     System.out.println("Before all test methods");
   }
 
   @BeforeEach
   void beforeEach(){
+
     System.out.println("Before each test methods");
   }
 
   @AfterEach
   void afterEach(){
+
     System.out.println("After each test methods");
   }
 
   @AfterAll
   static void afterAll(){
+
     System.out.println("After all test methods");
   }
 
   @Test
   @DisplayName("First test")
   void firstTest(){
+
     System.out.println("First test method");
   }
 
   @Test
   @DisplayName("Second test")
-  void secondTest(){ 
+  void secondTest(){
+    
     System.out.println("Second test method");
   }
 }
