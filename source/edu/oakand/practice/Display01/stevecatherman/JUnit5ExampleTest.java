48af00ba2eeb6c3ec21f1251a43a446db59f235c
stevecatherman <50778677+stevecatherman@users.noreply.github.com>
2020-11-04 21:48:27 -0500

    Update JUnit5ExampleTest.java

--- source/edu/oakand/practice/Display01/stevecatherman/JUnit5ExampleTest.java
+++ source/edu/oakand/practice/Display01/stevecatherman/JUnit5ExampleTest.java
@@ -3,41 +3,40 @@ package edu.oakland.practice.display01.stevecatherman;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.*;
 
 @DisplayName("JUnit 5 Example")
 class JUnit5ExampleTest {
 
-	@BeforeAll
-	static void beforeAll() {
-		System.out.println("Before all test methods");
-	}
+@BeforeAll
+static void beforeAll() {
+	System.out.println("Before all test methods");
+}
 
-	@BeforeEach
-	void beforeEach() {
-		System.out.println("Before each test method");
-	}
+@BeforeEach
+void beforeEach() {
+	System.out.println("Before each test method");
+}
 
-	@AfterEach
-	void afterEach() {
-		System.out.println("After each test method");
-	}
+@AfterEach
+void afterEach() {
+	System.out.println("After each test method");
+}
 
-	@AfterAll
-	static void afterAll() {
-		System.out.println("After all test methods");
-	}
+@AfterAll
+static void afterAll() {
+	System.out.println("After all test methods");
+}
 
-	@Test
-	@DisplayName("First test")
-	void firstTest() {
-		System.out.println("First test method");
-	}
+@Test
+@DisplayName("First test")
+void firstTest() {
+	System.out.println("First test method");
+}
 
-	@Test
-	@DisplayName("Second test")
-	void secondTest() {
-		System.out.println("Second test method");
-	}
+@Test
+@DisplayName("Second test")
+void secondTest() {
+	System.out.println("Second test method");
+}
 }
