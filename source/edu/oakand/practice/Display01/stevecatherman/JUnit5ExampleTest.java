eee89de6d27a86ddd343f4ebe052f547c448045c
stevecatherman <50778677+stevecatherman@users.noreply.github.com>
2020-11-04 23:05:51 -0500

    Update JUnit5ExampleTest.java

--- source/edu/oakand/practice/Display01/stevecatherman/JUnit5ExampleTest.java
+++ source/edu/oakand/practice/Display01/stevecatherman/JUnit5ExampleTest.java
@@ -2,12 +2,7 @@ package edu.oakland.practice.display01.stevecatherman;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 @DisplayName("JUnit 5 Example")
 public class Junit5ExampleTest {
