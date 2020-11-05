0daa5e6ec44edc7028908ae347c753175f9709e9
stevecatherman <50778677+stevecatherman@users.noreply.github.com>
2020-11-04 22:50:51 -0500

    Update JUnit5ExampleTest.java

--- source/edu/oakand/practice/Display01/stevecatherman/JUnit5ExampleTest.java
+++ source/edu/oakand/practice/Display01/stevecatherman/JUnit5ExampleTest.java
@@ -7,33 +7,33 @@ class Junit5ExampleTest {
 
 @BeforeAll
     static void beforeAll() {
-        System.out.println("Before all test methods");
-    }
+    System.out.println("Before all test methods");
+  }
 
 @BeforeEach
     void beforeEach() {
-        System.out.println("Before each test method");
-    }
+    System.out.println("Before each test method");
+  }
 
 @AfterEach
     void afterEach() {
-        System.out.println("After each test method");
-    }
+    System.out.println("After each test method");
+  }
 
 @AfterAll
     static void afterAll() {
-        System.out.println("After all test methods");
-    }
+    System.out.println("After all test methods");
+  }
 
 @Test
 @DisplayName("First test")
     void firstTest() {
-        System.out.println("First test method");
-    }
+    System.out.println("First test method");
+  }
 
 @Test
 @DisplayName("Second test")
     void secondTest() {
-        System.out.println("Second test method");
-    }
+    System.out.println("Second test method");
+  }
 }
