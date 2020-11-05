fd2e3fa89be32ff1db06b1275f2456cf0fee6a6d
Jessica Cortes <jcortes@oakland.edu>
2020-11-04 20:23:08 -0500

    Update JUnit5ExampleTest.java

--- source/edu/oakand/practice/display_02/jcortes18/JUnit5ExampleTest.java
+++ source/edu/oakand/practice/display_02/jcortes18/JUnit5ExampleTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("JUnit 5 Example")
-class Junit5exampletest {
+class JUnit5ExampleTest {
 
   @BeforeAll
   static void beforeAll() {
