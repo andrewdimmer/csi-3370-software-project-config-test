ab1ae9bf60f661a9eb060617e42967605a42f3d4
kwillard34 <60444686+kwillard34@users.noreply.github.com>
2020-11-08 09:56:20 -0500

    Update JunitPractice.java

--- source/edu/oakand/practice/middleware02/nbergamin123/JunitPractice.java
+++ source/edu/oakand/practice/middleware02/nbergamin123/JunitPractice.java
@@ -12,23 +12,23 @@ public class JunitPractice {
   static void beforeAll() {
     System.out.println("Before all test methods");
   }
-	
+
   @BeforeEach
   static void beforeEach() {
     System.out.println("Before each test method");
   }
-	
+
   @AfterEach
   static void afterEach() {
     System.out.println("After each test method");
   }
-	
+
   @Test
   @DisplayName("First test")
   void firstTest() {
     System.out.println("First test method");
   }
-	
+
   @Test
   @DisplayName("Second test")
   void secondTest() {
