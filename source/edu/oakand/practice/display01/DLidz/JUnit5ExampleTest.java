0c73fbf8ad06ebc6b82d4b25183f564def3e49c2
DLidz <46999371+DLidz@users.noreply.github.com>
2020-11-08 14:29:05 -0500

    Update JUnit5ExampleTest.java

--- source/edu/oakand/practice/display01/DLidz/JUnit5ExampleTest.java
+++ source/edu/oakand/practice/display01/DLidz/JUnit5ExampleTest.java
@@ -1,25 +1,25 @@
 import org.junit.jupiter.api.*;
 
 @DisplayName("JUnit 5 Example")
-class JUnit5ExampleTest {
+class Junit5ExampleTest {
 
 	@BeforeAll
 	static void beforeAll() {
-		System.out.println("Before all test methods");
+	System.out.println("Before all test methods");
 	}
 	
 	@BeforeEach
 	void beforeEach() {
-		System.out.println("Before each test method");
+	System.out.println("Before each test method");
 	}
 	
 	@AfterEach
 	void afterEach() {
-		System.out.println("After each test methods");
+	System.out.println("After each test methods");
 	}
 	
 	@AfterAll
 	static void afterAll() {
-		System.out.println("After all test methods");
+	System.out.println("After all test methods");
 	}
 }
