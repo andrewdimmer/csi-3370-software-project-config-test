99089f78b0d3370cd44908cd5992ecf34414d761
reedsikorskiOU <73550484+reedsikorskiOU@users.noreply.github.com>
2020-11-11 22:10:53 -0500

    Update RsJunitJava
    help2

--- source/edu/oakand/test/display01/RSJunit5ExampleTest.java
+++ source/edu/oakand/test/display01/RSJunit5ExampleTest.java
@@ -1,37 +1,37 @@
 import org.junit.jupiter.api.*;
 
-@DisplayName("JUnit 5 Example")
+@DisplayName("RsJunit5ExampleTest")
 class JUnit5ExampleTest {
-	
- @BeforeAll
+
+  @BeforeAll
  static void beforeAll() {
   System.out.println("Before all test methods");
  }
-	
-	
- @BeforeEach
+
+  @BeforeEach
  void beforeEach() {
-  System.out.println("Before each test method");
- }
-	
+    System.out.println("Before each test method");
+  }
+
  @AfterEach
  void afterEach() {
   System.out.println("After each test method");
  }
-	
- @AfterAll
+
+  @AfterAll
  static void afterAll() {
-  System.out.println("After all test methods");
- }
+    System.out.println("After all test methods");
+  }
 	
- @Test
+  @Test
  @DisplayName("First test")
  void firstTest() {
-  System.out.println("First test method");
- }
-	
- @Test
+    System.out.println("First test method");
+  }
+
+  @Test
  @DisplayName("Second test")
  void secondTest() {
-  System.out.println("Second test method");
- }}
+    System.out.println("Second test method");
+  }
+}
