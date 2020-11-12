9ada491ce7f0678b97dcb40e7a30380331108afb
reedsikorskiOU <73550484+reedsikorskiOU@users.noreply.github.com>
2020-11-11 22:59:27 -0500

    Update1

--- source/edu/oakand/test/display01/RSJunit5ExampleTest.java
+++ source/edu/oakand/test/display01/RSJunit5ExampleTest.java
@@ -1,4 +1,11 @@
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("RSJunit5ExampleTest")
 class RsJunit5ExampleTest {
