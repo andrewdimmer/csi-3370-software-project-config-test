1e9b75f58f97dea0e00fa3454fb5a2b854be56d4
reedsikorskiOU <73550484+reedsikorskiOU@users.noreply.github.com>
2020-11-16 00:25:16 -0500

    final2
    jhedsf

--- source/edu/oakand/test/display01/RSJunit5ExampleTest.java
+++ source/edu/oakand/test/display01/RSJunit5ExampleTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("RSJunit5ExampleTest")
-class RSJunit5ExampleTest {
+@DisplayName("RsJunit5ExampleTest")
+class RsJunit5ExampleTest {
 
   @BeforeAll
   static void beforeAll() {
