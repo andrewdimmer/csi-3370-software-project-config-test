da9713a40efb8b9c56ebc63badd802def611a493
robertsonbrinker <robertsonbrinker@gmail.com>
2020-11-19 14:21:31 -0500

    Update JUnit5ExampleTest.java

--- source/edu/oakand/practice/database/robertsonbrinker/JUnit5ExampleTest.java
+++ source/edu/oakand/practice/database/robertsonbrinker/JUnit5ExampleTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Junit Practice")
-public class JunitPractice {
+@DisplayName("JUnit Practice")
+public class JUnitPractice {
 
   @BeforeAll
   static void beforeAll() {
