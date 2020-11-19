356f40d8e9103633ca00707d1b1675da5be36322
robertsonbrinker <robertsonbrinker@gmail.com>
2020-11-19 14:08:08 -0500

    Update Junit5ExampleTest.java

--- source/edu/oakand/practice/database/robertsonbrinker/JUnit5ExampleTest.java
+++ source/edu/oakand/practice/database/robertsonbrinker/JUnit5ExampleTest.java
@@ -1,37 +1,37 @@
-import org.junit.jupiter.api;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-@DisplayName("JUnit 5 Example")
-class JUnit5ExampleTest {
+@DisplayName("Junit Practice")
+public class JunitPractice {
 
-    @BeforeAll
-    static void beforeAll() {
-        System.out.println("Before all test methods");
-    }
-	
-    @BeforeEach
-    void beforeEach() {
-        System.out.println("Before each test method");
-    }
+  @BeforeAll
+  static void beforeAll() {
+    System.out.println("Before all test methods");
+  }
 
-    @AfterEach
-    void afterEach() {
-        System.out.println("After each test method");
-    }
+  @BeforeEach
+  static void beforeEach() {
+    System.out.println("Before each test method");
+  }
 
-    @AfterAll
-    static void afterAll() {
-        System.out.println("After all test methods");
-    }
+  @AfterEach
+  static void afterEach() {
+    System.out.println("After each test method");
+  }
 
-    @Test
-    @DisplayName("First test")
-    void firstTest() {
-        System.out.println("First test method");
-    }
+  @Test
+  @DisplayName("First test")
+  void firstTest() {
+    System.out.println("First test method");
+  }
 
-    @Test
-    @DisplayName("Second test")
-    void secondTest() {
-        System.out.println("Second test method");
-    }
+  @Test
+  @DisplayName("Second test")
+  void secondTest() {
+    System.out.println("Second test method");
+  }
 }
