c441f470981ab58d1167ab37692a3739ef758d1e
Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
2020-11-22 21:39:14 -0500

    Update github_action_workflow_to_job_id.py
    Update the argument (the first argument is the file name... oops...), and break the loop after one match is found.

--- .github/github_action_workflow_to_job_id.py
+++ .github/github_action_workflow_to_job_id.py
@@ -2,5 +2,6 @@ import json
 import sys
 obj = json.load(sys.stdin)
 for job in obj["check_runs"]:
-    if job["name"] == sys.argv[0]:
+    if job["name"] == sys.argv[1]:
         print(job["id"])
+        break
